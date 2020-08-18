14348fc96b60f9b7f080112f77ea1e237ca3d062
xuhaudong <xuhuad_92@163.com>
2020-08-19 00:01:30 +0800

    20200819记录

--- consumer/src/main/java/com/sunyard/consumer/HelloController.java
+++ consumer/src/main/java/com/sunyard/consumer/HelloController.java
@@ -5,11 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 public class HelloController {
@@ -40,4 +48,43 @@ public class HelloController {
     public String hello2(){
        return restTemplateOne.getForObject("http://provider/hello1",String.class);
     }
+
+    @GetMapping("/hello3")
+    public String helloe(){
+        String s2 = restTemplateOne.getForObject("http://provider/restemplate?name={name}", String.class,"javaboy");
+        System.out.println(s2);
+        ResponseEntity<String> responseEntity = restTemplateOne.getForEntity("http://provider/restemplate?name={name}", String.class, "javagirl");
+        String body = responseEntity.getBody();
+        System.out.println("body:"+body);
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        System.out.println("statusCode:"+statusCode);
+        int statusCodeValue = responseEntity.getStatusCodeValue();
+        System.out.println("statusCodeValue:"+statusCodeValue);
+        HttpHeaders headers = responseEntity.getHeaders();
+        Set<String> strings = headers.keySet();
+        for (String string : strings) {
+            System.out.println(string+":"+headers.get(string));
+        }
+        return s2;
+    }
+    @GetMapping("/hello4")
+    public void helllo4() throws UnsupportedEncodingException {
+        //第一种方式
+        String s1 = restTemplate.getForObject("http://provider/restemplate?name={1}", String.class, "javaboy");
+        System.out.println(s1);
+        //第二种方式
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name","javaboy");
+        String s2 = restTemplate.getForObject("http://provider/restemplate?name={name}", String.class, map);
+        System.out.println(s2);
+        //第三种方式
+        String uriString = "http://provider/restemplate?name="+ URLEncoder.encode("张三","UTF-8");
+        URI uri = URI.create(uriString);
+        String s3 = restTemplate.getForObject(uri, String.class);
+        System.out.println(s3);
+
+    }
+
+
+
 }
--- provider/src/main/java/com/sunyard/provider/HelloController.java
+++ provider/src/main/java/com/sunyard/provider/HelloController.java
@@ -1,7 +1,10 @@
 package com.sunyard.provider;
 
+import com.sunyard.bean.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +15,20 @@ public class HelloController {
     public String hello(){
         return "hello springCloud"+port;
     }
+
+    @GetMapping("/restemplate")
+    public String helloRestemplate(String name){
+        return "this is restemolate"+ name ;
+    }
+
+    @PostMapping("/addUser1")
+    public User addUser(User user){
+        return user;
+    }
+
+    @PostMapping("/addUser2")
+    public User addUser2(@RequestBody User user){
+        return user;
+    }
+
 }
